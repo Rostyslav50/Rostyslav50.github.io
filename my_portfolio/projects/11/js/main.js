0e0823632d6f2f40ea04017dab5ebcf198fa8270
Rostyslav Karpiak <karpiak96@mail.ua>
2017-04-14 11:46:23 +0300

    demo

---
+++ my_portfolio/projects/11/js/main.js
@@ -0,0 +1,126 @@
+﻿
+
+
+$(document).ready(function(){
+	
+		
+				$("#form-zvonok").submit(function(e) {
+
+						var url = './js/sendmail.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									$('.form-bg .block-form').hide();
+									$('.form-bg .success').show();
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+				
+				$("#form-sumka").submit(function(e) {
+
+						var url = './js/sendmail2.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									$('.form-bg .block-form').hide();
+									$('.form-bg .success').show();
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+				
+				$("#form-komplekt").submit(function(e) {
+
+						var url = './js/sendmail3.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									$('.form-bg .block-form').hide();
+									$('.form-bg .success').show();
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+				
+				$('#myElement').click(function(){ 
+					$('.form-bg .block-form').show();
+					$('.form-bg .success').hide();
+					$('input[type="text"]').val('');
+				});
+				
+				$('#myElement2').click(function(){ 
+					$('.form-bg .block-form').show();
+					$('.form-bg .success').hide();
+					$('input[type="text"]').val('');
+				});
+				
+				$('#myElement3').click(function(){ 
+					$('.form-bg .block-form').show();
+					$('.form-bg .success').hide();
+					$('input[type="text"]').val('');
+				});
+				
+				
+				
+		$('.btn-pod').on('click', function(){
+		var $this = $(this);
+		$('.btn-pod').hide();
+		$('.text-hid').show();
+		return false;
+		});
+		
+		
+		$('.testimonials-foto a').lightBox();
+	
+});
+
+
+
+		$(function(){
+
+			var current_date = new Date();// текущая дата
+				current_date.setDate(current_date.getDate())
+				curDay = current_date.getDate(),
+				curMonth = current_date.getMonth(),
+				curYear = current_date.getFullYear();
+
+
+			$('#counter1').countMe(curYear, curMonth, curDay+1, '#counter1');
+			$('#counter2').countMe(curYear, curMonth, curDay+1, '#counter2');
+	 
+		})
+
+
+  
+  	$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top - 232
+				}, 1000);
+				return false;
+			  }
+			}
+		  });
+		});
+
+
+
+
+
---
+++ my_portfolio/projects/9/js/main.js
@@ -0,0 +1,187 @@
+﻿
+
+
+$(document).ready(function(){
+	
+		
+				$("#forma-regist").submit(function(e) {
+
+						var url = './js/sendmail.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									window.location.href = "./spasibo.html";
+									$('input[type="text"]').val('');
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+				
+				$("#forma-popap").submit(function(e) {
+
+						var url = './js/sendmail.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									window.location.href = "./spasibo.html";
+									$('input[type="text"]').val('');
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+				
+				$("#forma-popap2").submit(function(e) {
+
+						var url = './js/sendmail2.php';
+						$.ajax({
+							   type: "POST",
+							   url: url,
+							   data: $(this).serialize(), 
+							   success: function(data)
+							   {
+									window.location.href = "./spasibo.html";
+									$('input[type="text"]').val('');
+							   }
+							 });
+
+						e.preventDefault(); 
+				});	
+	
+
+				
+				
+				$('.remodal-close').click(function(){ 
+					$('.remodal .block-form').show();
+					$('.remodal .success').hide();
+					$('input[type="text"]').val('');
+				});
+				
+		
+		
+				$(".mask1").inputmask('+99(999)999-99-99');
+				
+			
+				
+		
+
+				var owl = $('#history-images');
+				owl.owlCarousel({
+					margin: 0,
+					responsiveClass:true,
+					loop:true,
+					slideSpeed: 500,
+					autoplay: false,
+					autoplayTimeout: 4500,
+					responsive: {
+						0: {
+							items: 1
+						},
+						600: {
+							items: 2
+						},
+						1024: {
+							items: 4
+						}
+					}
+				})
+				
+				var owl = $('#speakers-slide');
+				owl.owlCarousel({
+					margin: 0,
+					responsiveClass:true,
+					loop:true,
+					slideSpeed: 500,
+					autoplay: false,
+					autoplayTimeout: 4500,
+					responsive: {
+						0: {
+							items: 1
+						},
+						600: {
+							items: 2
+						},
+						1024: {
+							items: 4
+						}
+					}
+				})
+				
+						var owl = $('#program-slide');
+						owl.owlCarousel({
+							margin: 0,
+							responsiveClass:true,
+							loop:true,
+							slideSpeed: 500,
+							autoplay: false,
+							autoplayTimeout: 4500,
+							responsive: {
+								0: {
+									items: 1
+								},
+								600: {
+									items: 1
+								},
+								1024: {
+									items: 1
+								}
+							}
+						})
+						
+						
+				
+				
+				
+				
+	
+		
+				$('.foto-history .item a').lightBox();
+			
+				
+				$('.testimonial-single').click(function(e){
+					var data = $(this).data('video');
+					$('.video-inner').html('<iframe width="100%" height="500" src="'+ data +'" frameborder="0" allowfullscreen </iframe>');
+					$('#open-video').click();
+					$(".video-inner iframe")[0].src += "&autoplay=1";
+					
+
+				});
+				$('.remodal-close').click(function(e){
+					$('.video-inner iframe').remove();
+				});
+			
+});
+
+				
+
+
+
+  
+  	$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top 
+				}, 1000);
+				return false;
+			  }
+			}
+		  });
+		});
+
+		
+		
+
+
+
+
